999ac1633109c0f7858827407497496b764bb24a
niltonguilhem <nil.guilhem@gmail.com>
2023-02-28 19:11:39 -0300

    classe user resource findById

---
+++ src/main/java/br/com/ngtechnology/api/treinamento/resources/UserResource.java
@@ -0,0 +1,24 @@
+package br.com.ngtechnology.api.treinamento.resources;
+
+import br.com.ngtechnology.api.treinamento.domain.User;
+import br.com.ngtechnology.api.treinamento.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "/user")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+}

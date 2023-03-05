565f6dbfb9283155acb12f72dbc8b267ef03e21d
niltonguilhem <nil.guilhem@gmail.com>
2023-03-05 12:17:54 -0300

    endpoint create para user

--- src/main/java/br/com/ngtechnology/api/treinamento/services/UserService.java
+++ src/main/java/br/com/ngtechnology/api/treinamento/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.ngtechnology.api.treinamento.services;
 
 import br.com.ngtechnology.api.treinamento.domain.User;
+import br.com.ngtechnology.api.treinamento.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface UserService {
 
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/br/com/ngtechnology/api/treinamento/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/ngtechnology/api/treinamento/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.ngtechnology.api.treinamento.services.impl;
 
 import br.com.ngtechnology.api.treinamento.domain.User;
+import br.com.ngtechnology.api.treinamento.domain.dto.UserDTO;
 import br.com.ngtechnology.api.treinamento.repositories.UserRepository;
 import br.com.ngtechnology.api.treinamento.services.UserService;
 import br.com.ngtechnology.api.treinamento.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,10 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
@@ -23,5 +29,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 }

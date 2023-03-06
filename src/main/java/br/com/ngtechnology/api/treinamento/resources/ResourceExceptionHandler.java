cf7ed827dd87dc422f0c89e16e64081a2244d5ec
niltonguilhem <nil.guilhem@gmail.com>
2023-03-06 10:43:22 -0300

    excecao para email ja cadastrado na atualizacao

--- src/main/java/br/com/ngtechnology/api/treinamento/resources/ResourceExceptionHandler.java
+++ src/main/java/br/com/ngtechnology/api/treinamento/resources/ResourceExceptionHandler.java
@@ -23,7 +23,8 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(DataIntegratyViolationException.class)
-    public ResponseEntity<StandardError>dataIntegrayViolationException(DataIntegratyViolationException ex, HttpServletRequest request){
+    public ResponseEntity<StandardError>dataIntegrayViolationException(DataIntegratyViolationException ex,
+                                                                       HttpServletRequest request){
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(),
                 request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);

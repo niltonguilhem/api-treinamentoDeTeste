5454ae6a0b7bc0c7853bb449777884ac97b43b6a
niltonguilhem <nil.guilhem@gmail.com>
2023-03-06 15:47:16 -0300

    teste para classe principal

--- src/test/java/br/com/ngtechnology/api/treinamento/ApiTreinamentoApplicationTests.java
+++ src/test/java/br/com/ngtechnology/api/treinamento/ApiTreinamentoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApiTreinamentoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApiTreinamentoApplication.main(new String[]{});
 	}
 
 }
